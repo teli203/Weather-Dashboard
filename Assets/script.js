5b44293156831641301b9507fb96db48acc9869b
Telicia Watson <teli203@aol.com>
2020-10-01 22:17:43 -0500

    changes quotes to back tickers, took s off result

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 const api = {
 key: "094533805c4db4f536e9098a9a6d35cf",
-base: "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=094533805c4db4f536e9098a9a6d35cf"
+base: "https://api.openweathermap.org/data/2.5/"
 }
 
 const searchbox = document.querySelector('.search-box');
@@ -8,28 +8,30 @@ searchbox.addEventListener('keypress', setQuery);
 
 function setQuery (evt) {
     if (evt.keyCode == 13) {
-        getResults(searchbox.value);  
+        getResult(searchbox.value);  
     }
 }
 
 function getResult (query) {
-    fetch('${api.base}weather?q={query}&units=metric&APPID=${api.key}')
+    fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
     .then(weather => {
+        console.log(weather)
         return weather.json();
-    }).then(displayresults);
+    }).then(displayResults);
 }
 
 function displayResults (weather) {
+    console.log(weather)
     let city = document.querySelector('.location .city');
-    city.innerText = '${weather.name}, $weather.sys.country}';
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
 
     let temp = document.querySelector('.current .temp');
-    temp.innerHTML = '${Math.round(weather.main.temp)}<span>°c</span>';
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
 
 
     let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
 
-    let hilow = document.querySelector('hi-low');
-    hilow.innerText = '${Math.round(weather.main.temp_min}°c / ${Math.round(weather.main.temp_max)}°c';
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 }
